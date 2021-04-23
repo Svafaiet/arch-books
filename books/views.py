7153f1c3ec5495045ae7781c3f7b26dc34518bec
sajadgilga <majidstic@yahoo.com>
2021-04-24 00:00:06 +0430

    added permission class for books

--- books/views.py
+++ books/views.py
@@ -1,21 +1,17 @@
 from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-
 from rest_framework.viewsets import ModelViewSet
 
-
 from books.models import Book
+from books.permissions import BookPermission
 from books.serializers import BookSerializer
 
 
 class BooksAPIView(ModelViewSet):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
-    authentication_classes = []
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, BookPermission]
 
     def get(self, request):
-        user = request.user
-        print(user.token.payload['type'])
         return JsonResponse({}, status=status.HTTP_200_OK)
