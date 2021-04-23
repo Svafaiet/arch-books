0cacb9a12216537234e2c78663019ce9d3754d14
sajadgilga <majidstic@yahoo.com>
2021-04-23 22:07:11 +0430

    added jwt user auth

--- books/views.py
+++ books/views.py
@@ -4,8 +4,8 @@ from rest_framework.views import APIView
 
 
 class Books(APIView):
-    authentication_classes = []
     permission_classes = []
 
     def get(self, request):
+
         return JsonResponse({}, status=status.HTTP_200_OK)
