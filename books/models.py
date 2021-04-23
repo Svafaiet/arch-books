971acff0d28ef84f39adbe8decfcf8acc2616462
svafaiet <svafaiet@gmail.com>
2021-04-23 23:30:16 +0430

    Change in book model

--- books/models.py
+++ books/models.py
@@ -2,19 +2,19 @@ from django.db import models
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=256)
+    title = models.CharField(unique=True, max_length=256)
     author = models.CharField(max_length=256)
     tags = models.ManyToManyField("books.Tag", related_name="books", related_query_name='books')
 
 
 class Tag(models.Model):
-    DEFAULT_TAGS = [
-        ('sci-fi', 'علمی تخیلی'),
-        ('historical', 'تاریخی'),
-        ('educational', 'آموزشی'),
-        ('science', 'علمی'),
-        ('social', 'اجتماعی'),
-        ('novel', 'رمان'),
-    ]
 
-    name = models.CharField(max_length=64, choices=DEFAULT_TAGS, primary_key=True)
+    DEFAULT_TAGS = (
+        "social",
+        "novel",
+        "educational",
+        "science",
+        "sci-fi"
+    )
+
+    name = models.CharField(max_length=64, primary_key=True)
